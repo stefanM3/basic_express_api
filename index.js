97db01d6efcb929f9a87bf23b1859b92aa0b1985
Mutas <stefan.mutas@sogeti.com>
2021-02-11 19:08:43 +0100

    added db connection20

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const port = process.env.PORT || '3000';
 var MongoClient = require('mongodb').MongoClient;
 //const CosmosClient = require("@azure/cosmos").CosmosClient;
 //const client = new CosmosClient({ endpoint, key });
-//var assert = require('assert');
-//var ObjectId = require('mongodb').ObjectID;
-//var url = 'mongodb://stefandb3:AI06RGXX1rx8AQh36oV4I9GwwInECpEkpCsCAVQc16ZpEUQACn96vrc3iOVA3acB6NTUmUeB2CUAoehI1DQa9g%3D%3D@stefandb3.mongo.cosmos.azure.com:10255/?ssl=true&appName=@stefandb3@';
-var resString = 'Space7!';
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://stefandb3:AI06RGXX1rx8AQh36oV4I9GwwInECpEkpCsCAVQc16ZpEUQACn96vrc3iOVA3acB6NTUmUeB2CUAoehI1DQa9g%3D%3D@stefandb3.mongo.cosmos.azure.com:10255/?ssl=true&appName=@stefandb3@';
+var resString = 'Space9!';
 
 // Core module
 //var core = require('mongodb-core'),
@@ -45,29 +45,39 @@ var resString = 'Space7!';
 
 // mongoose.connect(mongoUri, { useMongoClient: true });
 
-
-
-
-
-
-
-
-
-
-
-
-/*var getCustomers = function (db, callback) {
+var getCustomers = function (db, callback) {
     var cursor = db.collection('customers').find();
     cursor.each(function (err, doc) {
-        assert.equal(err, null);
+      //  assert.equal(err, null);
         if (doc != null) {
             resString = resString + doc.id + '   ' + doc.givenname;
         } else {
-           // callback();
+            // callback();
         }
     });
 };
-*/
+
+
+MongoClient.connect(url, function (err, client) {
+    //assert.equal(null, err);
+    var db = client.db('customersdb');
+    
+        getCustomers(db, function () {
+            client.close();
+        });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 //added above
