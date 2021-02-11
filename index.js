928350e464c7402f55ced591198b2052eb1c3ef4
Mutas <stefan.mutas@sogeti.com>
2021-02-11 12:53:38 +0100

    added db connection2

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://stefandb3:AI06RGXX1rx8AQh36oV4I9GwwInECpEkpCsCAVQc16ZpEUQACn96vrc3iOVA3acB6NTUmUeB2CUAoehI1DQa9g%3D%3D@stefandb3.mongo.cosmos.azure.com:10255/?ssl=true&appName=@stefandb3@';
-var resString = "";
+var resString = ' ';
 
 var getCustomers = function (db, callback) {
     var cursor = db.collection('customers').find();
     cursor.each(function (err, doc) {
         assert.equal(err, null);
         if (doc != null) {
-            resString = resString + doc.id + "   " + doc.givenname
+            resString = resString + doc.id + '   ' + doc.givenname
         } else {
             callback();
         }
@@ -31,7 +31,7 @@ var getCustomers = function (db, callback) {
 
 app.get('/', (req, res) => {
 
-    res.send('Your Express API is up and running! and finished this tutorial eller?' + resString);
+    res.send('Your Express API is up and running! and finished this tutorial eller inte?' + resString);
 
 });
 
