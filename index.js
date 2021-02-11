273dfc430404edbe80492ad49db750a9575b081a
Mutas <stefan.mutas@sogeti.com>
2021-02-11 13:13:25 +0100

    added db connection6

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || '3000';
 
 //added below
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
+//var assert = require('assert');
+//var ObjectId = require('mongodb').ObjectID;
 //var url = 'mongodb://stefandb3:AI06RGXX1rx8AQh36oV4I9GwwInECpEkpCsCAVQc16ZpEUQACn96vrc3iOVA3acB6NTUmUeB2CUAoehI1DQa9g%3D%3D@stefandb3.mongo.cosmos.azure.com:10255/?ssl=true&appName=@stefandb3@';
 var resString = 'Space1';
 
